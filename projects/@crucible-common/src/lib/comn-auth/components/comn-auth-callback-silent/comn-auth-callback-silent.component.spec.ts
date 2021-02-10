ac502cc64a986e8288cea0723bc31f0232646e5b
sei-noconnor <72139157+sei-noconnor@users.noreply.github.com>
2021-02-10 11:08:34 -0500

    Upgrade angular to 11 (#9)
    Upgrade to angular 11

--- projects/@crucible-common/src/lib/comn-auth/components/comn-auth-callback-silent/comn-auth-callback-silent.component.spec.ts
+++ projects/@crucible-common/src/lib/comn-auth/components/comn-auth-callback-silent/comn-auth-callback-silent.component.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ComnAuthCallbackSilentComponent } from './comn-auth-callback-silent.component';
 
@@ -9,7 +9,7 @@ describe('CwdAuthCallbackSilentComponent', () => {
   let component: ComnAuthCallbackSilentComponent;
   let fixture: ComponentFixture<ComnAuthCallbackSilentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ComnAuthCallbackSilentComponent],
     }).compileComponents();
--- projects/cwd-common-app/src/app/posts/posts.component.spec.ts
+++ projects/cwd-common-app/src/app/posts/posts.component.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PostsComponent } from './posts.component';
 import { HttpClient, HttpHandler, HttpBackend } from '@angular/common/http';
@@ -14,7 +14,7 @@ describe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [PostsComponent],
       providers: [HttpClient, HttpHandler, HttpBackend, ComnSettingsService],
