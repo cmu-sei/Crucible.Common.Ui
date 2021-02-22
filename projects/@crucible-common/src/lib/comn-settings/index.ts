5bbfcc710619c6bfe79fb27d40520be2b515a9b7
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2021-02-22 16:33:52 -0500

    added missing export

--- projects/@crucible-common/src/lib/comn-settings/index.ts
+++ projects/@crucible-common/src/lib/comn-settings/index.ts
@@ -1,5 +1,6 @@
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
+export * from './models/comn-settings';
 export * from './services/comn-settings.service';
 export * from './comn-settings.module';
